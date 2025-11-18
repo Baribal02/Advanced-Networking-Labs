294b538efadaefab7a324559785dbd52ed74d43e
Baribal02 <63559240+Baribal02@users.noreply.github.com>
2025-11-18 11:31:16 +0100

    Update receiver.py

--- Lab 3/receiver.py
+++ Lab 3/receiver.py
@@ -5,7 +5,6 @@ import sys
 # implementation based on https://stackoverflow.com/questions/603852/how-do-you-udp-multicast-in-python
 GROUP = sys.argv[1]
 PORT = int(sys.argv[2])
-SCIPER = sys.argv[3]
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
