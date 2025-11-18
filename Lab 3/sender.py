9669afcfba68530d0a87e4ade293a843e4e63fd7
Baribal02 <63559240+Baribal02@users.noreply.github.com>
2025-11-18 11:29:02 +0100

    Update and rename multicast.py to sender.py

--- Lab 3/sender.py
+++ Lab 3/sender.py
@@ -1,4 +1,4 @@
-import sockets
+import socket
 import struct
 import sys
 
@@ -9,12 +9,13 @@ SCIPER = sys.argv[3]
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind((GROUP, PORT))
+#sock.bind((GROUP, PORT))
 
 mreq = struct.pack("4sl", socket.inet_aton(GROUP), socket.INADDR_ANY)
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+#sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
 while True:
     msg = input("Enter the message to send: ")
     msg = SCIPER.encode() + msg.encode()
     sock.sendto(msg, (GROUP, PORT))
+
