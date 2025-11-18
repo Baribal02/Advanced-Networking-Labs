a824c78415f7cc00cd1806ce0a6f3a22afddbd95
Baribal02 <63559240+Baribal02@users.noreply.github.com>
2025-11-18 11:16:57 +0100

    Update multicast.py

--- Lab 3/multicast.py
+++ Lab 3/multicast.py
@@ -3,8 +3,9 @@ import struct
 import sys
 
 # implementation based on https://stackoverflow.com/questions/603852/how-do-you-udp-multicast-in-python
-GROUP = "224.1.1.1"
-PORT = 10505
+GROUP = sys.argv[1]
+PORT = int(sys.argv[2])
+SCIPER = sys.argv[3]
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -14,5 +15,6 @@ mreq = struct.pack("4sl", socket.inet_aton(GROUP), socket.INADDR_ANY)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
 while True:
-    print(sock.recv(10240))
-
+    msg = input("Enter the message to send: ")
+    msg = SCIPER.encode() + msg.encode()
+    sock.sendto(msg, (GROUP, PORT))
